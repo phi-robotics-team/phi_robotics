eeba8d4550ed96ca2a748e340b7de12eacb10667
phi-robotics <phirobotics.452.4322@gmail.com>
2015-10-08 21:15:19 -0700

    I registered SaladQueen
    supposedly all the programs i registered got deleted. this is an
    important one so i registered it again.

--- code/alpha/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ code/alpha/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -58,9 +58,9 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register("NullOp", NullOp.class);
 
     manager.register("K9TeleOp", K9TeleOp.class);
-    manager.register ("PushBotAuto", PushBotAuto.class);
-    manager.register ("PushBotManual", PushBotManual.class);
-
+    manager.register("PushBotAuto", PushBotAuto.class);
+    manager.register("PushBotManual", PushBotManual.class);
+    manager.register("SaladQueen", SaladQueen.class);
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
